625f0364acd843b4bda711d2f3403eef30d686d7
Elzabé Els <elzzels25@gmail.com>
2023-08-17 11:26:29 +0200

    deleted components folder

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import MainLayout from '../components/MainLayout'
+import MainLayout from './components/MainLayout'
 
 export default function Home() {
   return (
