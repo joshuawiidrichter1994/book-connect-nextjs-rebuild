38bc94aaa7e9487ccb8941bd8405b71992b12c31
Elzabé Els <elzzels25@gmail.com>
2023-08-23 17:12:15 +0200

    removed event listeners exported dom cleaned index

--- components/bookPreview/bookPreview.js
+++ components/bookPreview/bookPreview.js
@@ -1,13 +1,81 @@
 import styles from "./bookPreview.module.css";
 import Image from "next/image";
+import { DOM } from "../../dom/dom";
 
+function BookPreview(props) {
+  const { Book, books, authors, BOOKS_PER_PAGE, genres } = props;
+  let { page, matches } = props;
 
-function BookPreview() {
+  const actions = {
+    list: {
+      updateRemaining: () => {
+        const initial = matches.length - page * BOOKS_PER_PAGE;
+        const hasRemaining = initial > 0;
+
+        const remaining = hasRemaining ? initial : 0;
+
+        DOM.list.button().disabled = !hasRemaining;
+        DOM.list.button().innerHTML = /* html */ `
+                <span>Show more</span>
+                <span class="list__remaining"> (${remaining})</span>
+            `;
+      },
+
+      increase: () => {
+        const newPage = page + 1;
+        const start = (newPage - 1) * BOOKS_PER_PAGE;
+        const end = newPage * BOOKS_PER_PAGE;
+
+        DOM.list
+          .items()
+          .appendChild(createPreviewsFragment(matches, [start, end]));
+        actions.list.updateRemaining();
+        page = newPage;
+      },
+
+      recreate: (display) => {
+        matches = display;
+        page = 1;
+
+        if (display.length < 1) {
+          DOM.list.message().classList.add("list__message_show");
+        } else {
+          DOM.list.message().classList.remove("list__message_show");
+        }
+
+        DOM.list.items().innerHTML = "";
+        const fragments = createPreviewsFragment(display, [0, 36]);
+        DOM.list.items().appendChild(fragments);
+        actions.list.updateRemaining();
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      },
+
+      close: () => {
+        DOM.list.active().open = false;
+      },
+
+      open: (event) => {
+        const previewId = event.target.dataset.preview;
+
+        if (previewId) {
+          const active = getBookById(previewId);
+          // Instead of opening a preview, navigate to the dynamic route
+          router.push(`/preview/${active.id}`);
+        }
+      },
+    },
+  };
+
   return (
     <dialog className={styles.overlay} data-list-active>
       <div className={styles.overlay__preview}>
-        <Image className={styles.overlay__blur} data-list-blur src="" alt=""/>
-        <Image className={styles.overlay__image} data-list-image src="" alt=""/>
+        <Image className={styles.overlay__blur} data-list-blur src="" alt="" />
+        <Image
+          className={styles.overlay__image}
+          data-list-image
+          src=""
+          alt=""
+        />
       </div>
       <div className={styles.overlay__content}>
         <h3 className={styles.overlay__title} data-list-title></h3>
@@ -21,6 +89,7 @@ function BookPreview() {
       <div className={styles.overlay__row}>
         <button
           className={`${styles.overlay__button} ${styles.overlay__button_primary}`}
+          onClick={actions.list.close}
           data-list-close
         >
           Close
--- components/themeSettings/themeSettings.js
+++ components/themeSettings/themeSettings.js
@@ -1,10 +1,65 @@
 import styles from "./themeSettings.module.css";
+import { DOM } from "../../dom/dom";
 
-function ThemeSettings() {
+function ThemeSettings(props) {
+  const { Book } = props;
+
+  const css = {
+    day: {
+      dark: "10, 10, 20",
+      light: "255, 255, 255",
+    },
+    night: {
+      dark: "255, 255, 255",
+      light: "10, 10, 20",
+    },
+  };
+
+  const actions = {
+    settings: {
+      cancel: () => {
+        DOM.settings.overlay().open = false;
+      },
+
+      submit: (event) => {
+        event.preventDefault();
+        const { theme } = convertSubmit(event);
+        setTheme(theme || "day");
+        DOM.settings.overlay().open = false;
+      },
+    },
+  };
+
+  /**
+   * @param {SubmitEvent} event
+   * @returns {object}
+   */
+  const convertSubmit = (event) => {
+    const formData = new FormData(event.target);
+    const result = Object.fromEntries(formData);
+    return result;
+  };
+
+  /**
+   * @param {'day' | 'night'} theme
+   */
+  const setTheme = (theme) => {
+    document.documentElement.style.setProperty("--color-dark", css[theme].dark);
+    document.documentElement.style.setProperty(
+      "--color-light",
+      css[theme].light
+    );
+  };
+
   return (
     <dialog className={styles.overlay} data-settings-overlay>
       <div className={styles.overlay__content}>
-        <form className={styles.overlay__form} data-settings-form id="settings">
+        <form
+          className={styles.overlay__form}
+          onSubmit={actions.settings.submit}
+          data-settings-form
+          id="settings"
+        >
           <label className={styles.overlay__field}>
             <div className={styles.overlay__label}>Theme</div>
 
@@ -20,7 +75,11 @@ function ThemeSettings() {
         </form>
 
         <div className={styles.overlay__row}>
-          <button className={styles.overlay__button} data-settings-cancel>
+          <button
+            className={styles.overlay__button}
+            onClick={actions.settings.cancel}
+            data-settings-cancel
+          >
             Cancel
           </button>
           <button
